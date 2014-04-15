6befae3f34af640aed757f71a0e18b179837a4dc
closure <retval@me.com>
2014-04-15 11:44:04 +0800

    Initial Commit

---
+++ Risp/Risp.h
@@ -0,0 +1,13 @@
+//
+//  Risp.h
+//  Risp
+//
+//  Created by closure on 4/15/14.
+//  Copyright (c) 2014 closure. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Risp : NSObject
+
+@end
---
+++ Risp/Risp.m
@@ -0,0 +1,13 @@
+//
+//  Risp.m
+//  Risp
+//
+//  Created by closure on 4/15/14.
+//  Copyright (c) 2014 closure. All rights reserved.
+//
+
+#import "Risp.h"
+
+@implementation Risp
+
+@end
